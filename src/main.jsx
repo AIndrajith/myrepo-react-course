4817e03c1ad1958857d568e76280886d1cc44e7d
AIndrajith <deegalla12378@usci.ruh.ac.lk>
2025-08-26 23:49:29 +0530

    chakra UI changes : Better UX

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import { ChakraProvider } from "@chakra-ui/react"
+import { system } from "../theme"
+import { Provider } from "@/components/ui/provider"
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ChakraProvider value={system}>
+      <Provider>
+        <App />
+      </Provider>
+    </ChakraProvider>
   </StrictMode>,
 )
