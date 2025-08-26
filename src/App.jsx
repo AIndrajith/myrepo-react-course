2d770383a819f34fec538bebbc76da82fd8aac08
AIndrajith <deegalla12378@usci.ruh.ac.lk>
2025-08-26 23:49:29 +0530

    chakra UI changes : Better UX

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { MainApp } from "./styled.js";
+import { Heading , Text , Button , Container, Center , Highlight } from "@chakra-ui/react"
 
 const App = () => {
   const storageKeyName = "awesomeApp";
@@ -14,14 +14,35 @@ const App = () => {
   },[count]);
 
   return (
-    <MainApp>
-      Hello World!
+    <>
 
-      <button onClick={() => addCount(count)}>
-        Count is {count}
-      </button>
+      <Container maxW={['full', 'container.lg']} mt={4} mb={4}>
+        <Center>
+          <Heading>
+            <Highlight
+              query="Stranger"
+              styles={{ px: "1",py: "1", color: "orange.fg" }}
+            >
+              Welcome Stranger ! 🎉
+            </Highlight>
+          </Heading>
+        </Center>
 
-    </MainApp>
+        <Center>
+          <Text fontStyle='italic' fontSize='md'>This is my beautiful Web App </Text>
+        </Center>
+        <Center>
+          <Text color="tomato" m={4} fontSize='lg' fontWeight='bold'>Count me Now!</Text>
+        </Center>
+
+        <Center>
+          <Button colorPalette='teal' onClick={() => addCount(count)}>
+            Count is {count}
+          </Button>
+        </Center>
+      </Container>
+
+    </>
   )
 }
 
